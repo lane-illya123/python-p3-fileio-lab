e2eff88d8d79adcefdc0e97cf9c9f0c9294b733d
illyalane <lane_illya@yahoo.com>
2024-04-18 23:34:27 -0400

    completed lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
+import os
+
 def write_file(file_name, file_content):
-    pass
+
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    
+    with open(f'{file_name}.txt', mode='a', encoding='utf-8') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_name = open('file_directory/file_name.txt', encoding='utf-8')
+    return file_name.read()
